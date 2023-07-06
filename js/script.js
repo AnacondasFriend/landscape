aa1cb84784dbac27b152d7b23a9bb6c1b2621ee2
Linka <angelok777100@mail.ru>
2023-07-06 11:23:46 +0300

    ну теперь точно исправлено масштабирование+добавлена сетка

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const nextButtons = document.querySelectorAll('.next');
 const backButtons = document.querySelectorAll('.back');
 const firstbtn = document.querySelectorAll('.firstForm__btn');
+
 let wrapper = document.querySelector('.wrapper');
 let notEmptyArea = document.querySelector('.notEmptyArea');
 let placeForCanva = document.querySelector('.placeForCanva');
+let gridChecks = document.querySelectorAll('.buttonsZone__btn')
 const canvaWidth = placeForCanva.getBoundingClientRect().width;
 const canvaHeight = placeForCanva.getBoundingClientRect().height;
 
@@ -15,6 +17,29 @@ const landWidth = document.querySelector('#landWidth');
 const landLength = document.querySelector('#landLength');
 
 let activeSlideIndex = 0;
+let oneMeter;
+
+
+function createGrids(h, width, height){
+    let stepsByWidth = Math.round(width/h);
+    let stepsByHeight = Math.round(height/h);
+    //console.log(width, height)
+    //console.log(stepsByWidth, stepsByHeight)
+    //console.log(oneMeter)
+    let resultString = '';
+    for(let i=1; i<stepsByWidth; i++){
+       // console.log(i)
+        step = i*oneMeter;
+        resultString += `<line x1="${step}" y1="0" x2="${step}" y2="${height*oneMeter}" style="stroke:rgb(255,0,0);stroke-width:2"/>`
+    }
+    for(let i=1; i<stepsByHeight; i++){
+        step = i*oneMeter;
+        resultString += `<line x1="0" y1="${step}" x2="${width*oneMeter}" y2="${step}" style="stroke:rgb(255,0,0);stroke-width:2"/>`
+    }
+    //console.log(resultString)
+    return resultString;
+}
+
 
 function sidesCompare(width, height){
     if(canvaHeight <= canvaWidth && height <= width){
@@ -30,6 +55,8 @@ function sidesCompare(width, height){
         return [height,width]; //ширина будет откладываться по длине
     }
 }
+
+
 function whichButton(slideNum){
     if(slideNum==0){
         areaWidth = landWidth.value; //это то шо в метрах
@@ -39,19 +66,28 @@ function whichButton(slideNum){
             return false;
         }
         else{
-            [areaWidth, areaLength]= sidesCompare(Number(areaWidth), Number(areaLength));    
+            console.log('before',areaWidth, areaLength);
+            [areaWidth, areaLength]= sidesCompare(Number(areaWidth), Number(areaLength));
+            aL = areaLength;
+            aW = areaWidth;
+            console.log('after', areaWidth, areaLength);    
             dif = canvaWidth/areaWidth;
             areaWidth *= dif; //это получается уже в пикселях
             areaLength *= dif;
-            console.log(areaWidth, areaLength)
+            console.log('dif', areaWidth, areaLength);
             if(areaLength > canvaHeight){ //выравниваем по длине
                 dif2 = canvaHeight/areaLength; 
                 console.log(dif2);
                 areaWidth *= dif2;
                 areaLength = canvaHeight;
+                console.log('dif2', areaWidth, areaLength);
             }
-            console.log(areaWidth, areaLength)
-            placeForCanva.insertAdjacentHTML('afterbegin', `<svg width='${areaWidth}px' height='${areaLength}px'> <rect width='${areaWidth}px' height='${areaLength}px' fill='#71f37194' stroke-width='1' stroke-color='black'/></svg>`)
+            oneMeter = areaWidth / aW;
+            oneMeter2 = areaLength / aL;
+            console.log(oneMeter, oneMeter2)
+            h=1;
+            gridLines = createGrids(h, aW, aL)
+            placeForCanva.insertAdjacentHTML('afterbegin', `<svg width='${areaWidth}px' height='${areaLength}px'> <rect width='${areaWidth}px' height='${areaLength}px' style="fill:#71f37194; stroke-width:2; stroke:black"/>${gridLines}</svg>`)
             return true;
         }
     }
@@ -60,6 +96,8 @@ function whichButton(slideNum){
     }
     return true;
 }
+
+
 emptyCheck.addEventListener('change', (e)=>{
     if(e.target.checked){
         woodCheck.setAttribute('disabled','');
@@ -100,8 +138,7 @@ for(button of nextButtons){
         activeSlideIndex++;    
         wrapper.style.top = `${-height*activeSlideIndex}px`;             
         }        
-     
-    })
+ })
 }
 
 for(button of backButtons){
